7de98f67680040107371dd653a9487f53ab38e1e
RANJAN <aryaprakashranjan@gmail.com>
2026-03-01 19:34:31 +0530

    Inject UserRepository in checkUpdateRoute and optimize initial auth request by checking email existence

--- src/main/kotlin/com/ranjan/server/update/UpdateRoute.kt
+++ src/main/kotlin/com/ranjan/server/update/UpdateRoute.kt
@@ -1,11 +1,14 @@
 package com.ranjan.server.update
 
+import com.ranjan.domain.auth.repository.UserRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.respond
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Application.checkUpdateRoute() {
+    val userRepository by inject<UserRepository>()
     routing {
         route("/"){
             get{
@@ -17,6 +20,7 @@ fun Application.checkUpdateRoute() {
         }
         route("/checkUpdate") {
             get {
+                userRepository.isEmailExists("") // waking database to reduce latency for first auth request
                 call.respond(
                     HttpStatusCode.OK,
                     false
