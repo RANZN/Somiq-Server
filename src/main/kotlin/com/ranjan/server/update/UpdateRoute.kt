b239fe79509a06b6d6aef76f01a1b5d63d0bbbab
Prakash Ranjan <aryaprakashranjan@gmail.com>
2026-09-19 01:13:34 +0530

    Merge pull request #10
    * Migrating from Render to Google cloud Run: Update checkUpdateRoute an…

--- src/main/kotlin/com/ranjan/server/update/UpdateRoute.kt
+++ src/main/kotlin/com/ranjan/server/update/UpdateRoute.kt
@@ -1,31 +1,23 @@
 package com.ranjan.server.update
 
-import com.ranjan.domain.auth.repository.UserRepository
-import io.ktor.http.HttpStatusCode
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.respond
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.koin.ktor.ext.inject
 
 fun Application.checkUpdateRoute() {
-    val userRepository by inject<UserRepository>()
     routing {
-        route("/"){
-            get{
-                call.respond(
-                    HttpStatusCode.OK,
-                    "Welcome RANJAN"
-                )
-            }
+        get("/") {
+            call.respond(
+                HttpStatusCode.OK,
+                "Welcome RANJAN"
+            )
         }
-        route("/checkUpdate") {
-            get {
-                userRepository.isEmailExists("") // waking database to reduce latency for first auth request
-                call.respond(
-                    HttpStatusCode.OK,
-                    false
-                )
-            }
+        get("/checkUpdate") {
+            call.respond(
+                HttpStatusCode.OK,
+                false
+            )
         }
     }
 }
