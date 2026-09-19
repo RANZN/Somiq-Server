005ef28f1b863c13cd8575cb4cc089f1321b2d1a
RANJAN <aryaprakashranjan@gmail.com>
2026-09-19 17:26:08 +0530

    refactor(plugin): extract Ktor server plugins into core.plugin preserving git history

---
+++ core/src/main/kotlin/com/ranjan/core/plugin/CorsPlugin.kt
@@ -0,0 +1,30 @@
+package com.ranjan.core.plugin
+
+import com.ranjan.core.config.AppEnv
+import com.ranjan.core.config.CorsConfig
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCORS(config: CorsConfig = CorsConfig(AppEnv.LOCAL)) {
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Post)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.ContentType)
+
+        if (config.env.isProduction) {
+            config.allowedHosts.forEach { host ->
+                allowHost(
+                    host = host,
+                    schemes = listOf("http", "https")
+                )
+            }
+        } else {
+            anyHost()
+        }
+    }
+}
